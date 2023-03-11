b15c8397a1f45cbd9e94e25193ed40c8fbf05b7e
dupin_jade <jadedupin00@gmail.com>
2023-03-11 15:37:35 +0100

    modif classe rdv

--- src/main/java/com/intiFormation/entity/RendezVous.java
+++ src/main/java/com/intiFormation/entity/RendezVous.java
@@ -11,8 +11,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import org.springframework.format.annotation.DateTimeFormat;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
